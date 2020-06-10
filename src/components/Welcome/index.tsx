5594c28dae14f8ebaafe11725ab0218b64c8bbce
Daute Rodríguez Rodríguez <alu0100973914@ull.edu.es>
2020-06-10 18:49:43 +0100

    Ref: Welcome view

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -4,9 +4,17 @@ import {
   Typography,
   Theme,
   useTheme,
-  useMediaQuery
+  useMediaQuery,
+  Card,
+  Grid,
+  GridSize,
+  Container
 } from '@material-ui/core';
 import Logo from '../Logo';
+import TruckIconPNG from '../../assets/TruckIcon.png';
+import FactoryIconPNG from '../../assets/FactoryIcon.png';
+import RetailerIconPNG from '../../assets/RetailerIcon.png';
+import WarehouseIconPNG from '../../assets/WarehouseIcon.png';
 
 const useStyles = makeStyles<Theme>((theme: Theme) => ({
   root: () => ({
@@ -18,7 +26,7 @@ const useStyles = makeStyles<Theme>((theme: Theme) => ({
     marginBottom: theme.spacing(1)
   }),
   subtitle: () => ({
-    marginBottom: theme.spacing(4)
+    marginBottom: theme.spacing(8)
   }),
   name: () => ({
     fontSize: 34,
@@ -27,17 +35,99 @@ const useStyles = makeStyles<Theme>((theme: Theme) => ({
       fontSize: 48
     }
   }),
-  logo: () => ({
-    marginTop: theme.spacing(4),
-    width: 200,
-    [theme.breakpoints.up('sm')]: {
-      width: 300
-    },
+  iconCard: () => ({
+    borderRadius: '50%',
+    height: 0,
+    width: '100%',
+    paddingBottom: '100%'
+  }),
+  icon: () => ({
+    width: '100%',
+    height: 'auto'
+  }),
+  gridRoot: () => ({
+    flexGrow: 1,
     marginBottom: theme.spacing(4)
+  }),
+  gridItem: () => ({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
   })
 }));
 
+interface GridItemType {
+  isIcon: boolean;
+  element: string;
+  size: GridSize;
+}
+
+const GridItems: GridItemType[] = [
+  {
+    isIcon: true,
+    element: FactoryIconPNG,
+    size: 3
+  },
+  {
+    isIcon: false,
+    element:
+      'Factories and manufactures registry their goods in the chain, allowing product tracking to supply chain agents',
+    size: 9
+  },
+  {
+    isIcon: false,
+    element:
+      'Transport companies Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu tortor ultricies sem vulputate aliquam. Etiam ligula odio, blandit vitae lectus sit amet, feugiat sagittis magna. ',
+    size: 9
+  },
+  {
+    isIcon: true,
+    element: TruckIconPNG,
+    size: 3
+  },
+
+  {
+    isIcon: true,
+    element: WarehouseIconPNG,
+    size: 3
+  },
+  {
+    isIcon: false,
+    element:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu tortor ultricies sem vulputate aliquam. Etiam ligula odio, blandit vitae lectus sit amet, feugiat sagittis magna. ',
+    size: 9
+  },
+  {
+    isIcon: false,
+    element:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu tortor ultricies sem vulputate aliquam. Etiam ligula odio, blandit vitae lectus sit amet, feugiat sagittis magna. ',
+    size: 9
+  },
+  {
+    isIcon: true,
+    element: RetailerIconPNG,
+    size: 3
+  }
+];
+
+const GridItem: React.FC<GridItemType> = (props) => {
+  const classes = useStyles();
+  return (
+    <Grid className={classes.gridItem} item xs={props.size}>
+      {props.isIcon ? (
+        <Card className={classes.iconCard}>
+          <img className={classes.icon} src={props.element}></img>
+        </Card>
+      ) : (
+        <Typography>{props.element}</Typography>
+      )}
+    </Grid>
+  );
+};
+
 interface Props {}
+
+//FIXME: Horizontal scroll CSS issue
 
 export const Welcome: React.FC<Props> = (props) => {
   const classes = useStyles();
@@ -59,6 +149,13 @@ export const Welcome: React.FC<Props> = (props) => {
       <Typography align="center" variant="h6" className={classes.subtitle}>
         Blockchain applied to supply chain orchestration.
       </Typography>
+      <Container maxWidth="md" className={classes.gridRoot}>
+        <Grid container spacing={3}>
+          {GridItems.map((item, index) => (
+            <GridItem key={index} {...item} />
+          ))}
+        </Grid>
+      </Container>
     </div>
   );
 };
