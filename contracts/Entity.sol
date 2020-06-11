20187572103010b15ad15d2e3946130e83d272e6
Daute Rodríguez Rodríguez <alu0100973914@ull.edu.es>
2020-06-11 16:54:12 +0100

    Fix: None EntityType as default value

--- contracts/Entity.sol
+++ contracts/Entity.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 contract Entity {
-  enum EntityType { Admin, Factory, Transport, Warehouse, Retailer }
+  enum EntityType { None, Admin, Factory, Transport, Warehouse, Retailer }
 
   struct EntityData {
     string name;
