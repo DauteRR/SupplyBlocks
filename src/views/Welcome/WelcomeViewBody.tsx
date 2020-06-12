028481ff28d63bff86057c25623c0eeb55ee5a27
Daute Rodríguez Rodríguez <alu0100973914@ull.edu.es>
2020-06-12 12:30:08 +0100

    Add: welcome view advantages section

--- src/views/Welcome/WelcomeViewBody.tsx
+++ src/views/Welcome/WelcomeViewBody.tsx
@@ -3,7 +3,6 @@ import {
   makeStyles,
   Theme,
   useTheme,
-  useMediaQuery,
   Typography,
   Grid
 } from '@material-ui/core';
@@ -11,6 +10,13 @@ import FactoryPNG from '../../assets/factory.png';
 import WarehousePNG from '../../assets/warehouse.png';
 import RetailerPNG from '../../assets/retailer.png';
 import TransportPNG from '../../assets/transport.png';
+import BoxesPNG from '../../assets/boxes.png';
+import PresentationPNG from '../../assets/business-presentation.png';
+import TrustPNG from '../../assets/handshake.png';
+import TrackingPNG from '../../assets/product-delivery-tracking.png';
+import VerifiedPNG from '../../assets/result-pass.png';
+import SecurityPNG from '../../assets/security.png';
+import SupplyChainPNG from '../../assets/supply-chain.png';
 
 const useStyles = makeStyles<Theme>((theme: Theme) => ({
   advantages: {
@@ -32,7 +38,11 @@ const useStyles = makeStyles<Theme>((theme: Theme) => ({
   },
   agentsTitle: {
     color: 'white',
-    marginBottom: theme.spacing(4)
+    margin: theme.spacing(4, 0)
+  },
+  advantagesTitle: {
+    color: theme.palette.primary.main,
+    margin: theme.spacing(4, 0)
   },
   sentence: {
     color: 'white'
@@ -45,7 +55,7 @@ const useStyles = makeStyles<Theme>((theme: Theme) => ({
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'column',
-    margin: theme.spacing(2)
+    margin: theme.spacing(0, 6, 6)
   },
   itemImage: {
     width: 126,
@@ -55,18 +65,67 @@ const useStyles = makeStyles<Theme>((theme: Theme) => ({
     alignItems: 'flex-end',
     margin: theme.spacing(2)
   },
-  itemLabel: {
+  agentLabel: {
     color: 'white'
+  },
+  advantageLabel: {
+    color: theme.palette.primary.main
   }
 }));
 
-interface ItemProps {
+interface Item {
   label: string;
   src: string;
   alt: string;
+  text?: string;
 }
 
-const agents: ItemProps[] = [
+const advantages: Item[] = [
+  {
+    label: 'Lorem',
+    src: TrustPNG,
+    alt: 'IPSUM',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus placerat leo, sed egestas libero malesuada at.'
+  },
+  {
+    label: 'Lorem',
+    src: SecurityPNG,
+    alt: 'IPSUM',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus placerat leo, sed egestas libero malesuada at.'
+  },
+  {
+    label: 'Lorem',
+    src: BoxesPNG,
+    alt: 'IPSUM',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus placerat leo, sed egestas libero malesuada at.'
+  },
+  {
+    label: 'Lorem',
+    src: PresentationPNG,
+    alt: 'IPSUM',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus placerat leo, sed egestas libero malesuada at.'
+  },
+  {
+    label: 'Lorem',
+    src: TrackingPNG,
+    alt: 'IPSUM',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus placerat leo, sed egestas libero malesuada at.'
+  },
+  {
+    label: 'Lorem',
+    src: VerifiedPNG,
+    alt: 'IPSUM',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus placerat leo, sed egestas libero malesuada at.'
+  }
+];
+
+const agents: Item[] = [
   {
     label: 'Factory',
     src: FactoryPNG,
@@ -89,17 +148,22 @@ const agents: ItemProps[] = [
   }
 ];
 
+interface ItemProps extends Item {
+  typographyClassname: string;
+}
+
 const Item: React.FC<ItemProps> = (props) => {
   const classes = useStyles();
-  const { label, ...rest } = props;
+  const { label, typographyClassname, ...rest } = props;
   return (
     <div className={classes.item}>
       <div className={classes.itemImage}>
         <img {...rest}></img>
       </div>
-      <Typography className={classes.itemLabel} variant="h5" align="center">
+      <Typography className={typographyClassname} variant="h5" align="center">
         {label}
       </Typography>
+      {props.text && <Typography align="center">{props.text}</Typography>}
     </div>
   );
 };
@@ -117,7 +181,29 @@ const Agents: React.FC = (props) => {
       <Grid className={classes.itemsGrid} container>
         {agents.map((agent, index) => (
           <Grid key={index} item xs={12} sm={6} lg={3}>
-            <Item {...agent} />
+            <Item typographyClassname={classes.agentLabel} {...agent} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+const Advantages: React.FC = (props) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.advantages}>
+      <Typography
+        className={classes.advantagesTitle}
+        align="center"
+        variant="h4"
+      >
+        Advantages
+      </Typography>
+      <Grid className={classes.itemsGrid} container>
+        {advantages.map((agent, index) => (
+          <Grid key={index} item xs={12} sm={6} lg={4}>
+            <Item typographyClassname={classes.advantageLabel} {...agent} />
           </Grid>
         ))}
       </Grid>
@@ -133,7 +219,7 @@ export const WelcomeViewBody: React.FC<Props> = (props) => {
 
   return (
     <>
-      <div className={classes.advantages}></div>
+      <Advantages />
       <Agents />
     </>
   );
