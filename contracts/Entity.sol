32cf9a28e2fcc9973b84a09aad7ecfb39ebd3295
Daute Rodríguez Rodríguez <alu0100973914@ull.edu.es>
2020-06-14 03:29:58 +0100

    Fix: Smart Contract bug

--- contracts/Entity.sol
+++ contracts/Entity.sol
@@ -4,23 +4,31 @@ import './TypesLibrary.sol';
 contract Entity {
   using TypesLib for TypesLib.EntityType;
 
+  address public id;
   string public name;
   string public email;
   string public phoneNumber;
   TypesLib.EntityType public entityType;
   bool public set;
+  bool public approved;
 
   constructor(
     string memory _name,
     string memory _email,
     string memory _phoneNumber,
     TypesLib.EntityType _type,
-    bool _set
+    address _id
   ) public {
+    id = _id;
     name = _name;
     email = _email;
     phoneNumber = _phoneNumber;
     entityType = _type;
-    set = _set;
+    set = true;
+    approved = false;
+  }
+
+  function setApproved(bool _approved) public {
+    approved = _approved;
   }
 }
--- contracts/Manager.sol
+++ contracts/Manager.sol
@@ -6,8 +6,9 @@ import './Product.sol';
 contract Manager {
   using TypesLib for TypesLib.EntityType;
 
-  Entity supplyBlocks;
-
+  address[] public accounts;
+  Entity[] public entities;
+  mapping(address => bool) public setEntities;
   mapping(address => bool) public approvedEntities;
   mapping(address => Entity) public entitiesMapping;
 
@@ -20,19 +21,20 @@ contract Manager {
   mapping(address => bool) public registeredDeliveries;
 
   constructor() public {
-    supplyBlocks = new Entity(
-      'SupplyBlocks',
-      'supplyblocks@ull.edu.es',
-      '123456789',
-      TypesLib.EntityType.Admin,
-      true
+    entities.push(
+      new Entity(
+        'SupplyBlocks',
+        'supplyblocks@ull.edu.es',
+        '123456789',
+        TypesLib.EntityType.Admin,
+        msg.sender
+      )
     );
-    entitiesMapping[msg.sender] = supplyBlocks;
+    accounts.push(msg.sender);
+    entitiesMapping[msg.sender] = entities[entities.length - 1];
     approvedEntities[msg.sender] = true;
-  }
-
-  function checkAccount(address _address) public view returns (bool) {
-    return (entitiesMapping[_address].set());
+    setEntities[msg.sender] = true;
+    entitiesMapping[msg.sender].setApproved(true);
   }
 
   function createEntity(
@@ -45,14 +47,11 @@ contract Manager {
       _type != TypesLib.EntityType.Admin,
       'Trying to create an Admin user'
     );
-    require(!checkAccount(msg.sender), 'Account already registered');
-    entitiesMapping[msg.sender] = new Entity(
-      _name,
-      _email,
-      _phoneNumber,
-      _type,
-      true
-    );
+    require(!setEntities[msg.sender], 'Account already registered');
+    entities.push(new Entity(_name, _email, _phoneNumber, _type, msg.sender));
+    accounts.push(msg.sender);
+    entitiesMapping[msg.sender] = entities[entities.length - 1];
+    setEntities[msg.sender] = true;
   }
 
   function createProduct(string memory _name) public {
@@ -74,12 +73,13 @@ contract Manager {
   }
 
   function approveEntity(address _address) public {
-    require(checkAccount(_address), 'Entity not registered');
+    require(entitiesMapping[_address].set(), 'Entity not registered');
     require(
       entitiesMapping[msg.sender].entityType() == TypesLib.EntityType.Admin,
       'Unauthorized'
     );
 
     approvedEntities[_address] = true;
+    entitiesMapping[_address].setApproved(true);
   }
 }
